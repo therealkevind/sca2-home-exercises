5e66764b0efba9ff80a8c942d5ba6fad9fd4a42a
therealkevind <47948540+therealkevind@users.noreply.github.com>
2020-07-06 16:52:18 -0400

    day 1 solutions

--- day1/drills.js
+++ day1/drills.js
@@ -1,47 +1,67 @@
-const assert = require("assert")
-
-// Feel free to look things up online!
+const assert = require("assert");
 
 const reverse = function(str) {
-  // TODO - write a function which reverses the string
-}
+  return str.split("").reverse().join("");
+};
 
 const factorial = function(num) {
-  // TODO - write a function which returns the factorial of a positive integer
-  // For example, 5! = 5 * 4 * 3 * 2 * 1 = 120
-}
+  return Array.from({length: num}, (_, i) => i+1).reduce((a, i) => a*i);
+};
 
-const announceDate = function() {
-  // TODO - write a function which returns the current date in the following format:
-  // "Today's date is January 7th, 2016. It is eleven thirty-seven in the morning."
-}
+const announceDate = (()=>{
+  const digits = "one two three four five six seven eight nine".split(" ");
+  const hourNames = ["twelve", ...digits, "eleven"];
+  const minNames = ["o'clock"].concat(
+    digits.map(i=>`oh-${i}`),
+    "eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen".split(" "),
+    ...["twenty", "thirty", "forty", "fifty"].map(i => [i, ...digits.map(e => `${i}-${e}`)]),
+  );
+  return function() {
+    const now = new Date();
+    const hours = now.getHours(),
+      mins = now.getMinutes();
+    return `Today's date is ${
+      now.toLocaleDateString("en-US", {month: "long", day: "numeric", year: "numeric"})
+    }. It is ${hourNames[hours % 12]} ${minNames[mins]} ${
+      hours < 6 || hours > 20 ? "at night"
+      : hours < 12 ? "in the morning"
+      : hours < 18 ? "in the afternoon" : "in the evening"
+    }.`;
+  };
+})();
 
 const shiftRight = function(str, num) {
-  // TODO - write a function which shifts a string `num` characters to the right
-  // Example = shiftThree("Hello") === "lloHe"
-}
+  return str.substring(str.length - num) + str.substring(0, str.length - num);
+};
 
 const tokenize = function(str) {
-  // TODO - write a function which converts a multi-word string into an array of words
-}
+  return str.split(/\W+/g).filter(i => i);
+};
 
 const uniqueOnes = function(ary) {
-  // TODO - write a function which returns the inputted array without duplicate elements
-}
+  return Array.from(new Set(ary));
+};
 
 const zip = function(arr1, arr2) {
-  // TODO - write a function which combines two arrays into an array of 2-element arrays and returns -1
-  // if the two arrays are of unequal length
-  // Example: zip([1, 2, 3], [5, 3, 1]) === [[1, 5], [2, 3], [3, 1]]
-}
+  return arr1.length === arr2.length ? arr1.map((e, i) => [e, arr2[i]]) : -1;
+};
 
 const unzip = function(arr) {
-  // TODO - Write a function which does the opposite of `zip()`
-}
+  return Array.from({length: 2}, (_, i) => Array.from(arr, e=>e[i]));
+};
 
 // Write tests here:
 
-assert(1 < 2)
-assert(1 + 2 == 3)
-assert([2, 3][0] === 2)
-// assert(reverse("3df") === "fd3")
+assert.strictEqual(reverse("3df"), "fd3");
+assert.strictEqual(factorial(5), 120);
+assert.strictEqual(shiftRight("Hello", 3), "lloHe");
+assert.deepEqual(tokenize("Hello, world!"), ["Hello", "world"]);
+assert.deepEqual(uniqueOnes([1,2,1,1,3,4,2,5,3,4,8,7]),[1,2,3,4,5,8,7]);
+assert.deepEqual(zip([1, 2, 3], [5, 3, 1]), [[1, 5], [2, 3], [3, 1]]);
+assert.strictEqual(zip([1, 2, 3], [5, 3]), -1);
+assert.strictEqual(zip([1, 2], [5, 3, 1]), -1);
+assert.deepEqual(unzip([[1, 5], [2, 3], [3, 1]]), [[1, 2, 3], [5, 3, 1]]);
+assert.deepEqual(unzip(zip([], [])), [[], []]);
+
+console.log(announceDate());
+console.log("All assertions passed");
