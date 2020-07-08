bebd794483cf368b61dae85bf73f3f3d8c0f65fc
therealkevind <47948540+therealkevind@users.noreply.github.com>
2020-07-08 15:15:23 -0400

    day 3 solutions

--- day3/index.js
+++ day3/index.js
@@ -1,16 +1,22 @@
-const express = require("express")
-const path = require("path")
-const app = express()
+const express = require("express");
+const path = require("path");
+const app = express();
 
-app.use(express.static(__dirname + "/public"))
-const views = path.join(__dirname, "views")
+app.use(express.static(`${__dirname}/public`));
+const views = path.join(__dirname, "views");
 
 app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: views })
-})
+  res.sendFile("index.html", { root: views });
+});
 app.get("/about", (req, res) => {
-  res.sendFile("about.html", { root: views })
-})
+  res.sendFile("about.html", { root: views });
+});
+app.get("/js", (req, res) => {
+  res.sendFile("js.html", { root: views });
+});
+app.get("/py", (req, res) => {
+  res.sendFile("py.html", {root: views});
+});
 
-app.listen(3000)
-console.log("Running at port 3000")
+app.listen(3000);
+console.log("Running at port 3000");
